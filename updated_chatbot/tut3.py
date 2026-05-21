b3fa5efb587fe5ede50a82d8909b168699099d15
tenoob <18bcs6218@gmail.com>
2026-05-21 23:01:13 +0530

    updated chatbout development

---
+++ updated_chatbot/tut3.py
@@ -0,0 +1,36 @@
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_openai import ChatOpenAI
+
+
+import os
+import streamlit as st
+from dotenv import load_dotenv
+load_dotenv()
+os.environ["LANGCHAIN_API_KEY"] = os.getenv("LANGCHAIN_API_KEY")
+os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
+os.environ["LANGCHAIN_TRACING_V2"] = "true"
+
+
+#prompt templete
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system","You are an assistent. Help with the questions"),
+        ("user,Question:{question}")
+    ]
+)
+
+#streamlit framework
+st.title("Test 1")
+input_text = st.text_input("Give input")
+
+#openai
+llm = ChatOpenAI(temperature=)
+output_parser = StrOutputParser()
+chain = prompt | llm |output_parser
+
+if input_text:
+    st.write(chain.invoke({'question':input_text}))
+
+print("df")
+
